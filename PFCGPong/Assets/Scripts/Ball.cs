169117797d31db9b65d89b3bae33f05b940be038
Kemxu <bportelli1@gmail.com>
2017-12-18 11:03:06 +0100

    Update Ball.cs

--- PFCGPong/Assets/Scripts/Ball.cs
+++ PFCGPong/Assets/Scripts/Ball.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	//ball will be moved by 4.5force to a direction//
     [SerializeField]
     float forceValue = 4.5f;
     Rigidbody2D body;
-
+	
+	//this will allow that when the game starts, the ball will move in a direction//
 	void Start () {
         body = GetComponent<Rigidbody2D>();
         body.AddForce(new Vector2(forceValue * 50, 50));
