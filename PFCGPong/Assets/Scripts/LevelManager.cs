0cf3f04561278d41f04569011f0b6640b36c2727
Kemxu <bportelli1@gmail.com>
2017-12-18 11:05:05 +0100

    Update LevelManager.cs

--- PFCGPong/Assets/Scripts/LevelManager.cs
+++ PFCGPong/Assets/Scripts/LevelManager.cs
@@ -5,21 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+    //sets the level loading scenario//
     public void LoadLvl(string lvlName)
     {
         SceneManager.LoadScene(lvlName);
     }
 
+    //this will allow the next level to be loaded//
     public void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //StartGame function that will load the level//
     public void StartGame()
     {
         LoadNextLevel();
     }
 
+    //QuitGame will stop the game but not exit unity//
     public void QuitGame()
     {
         UnityEditor.EditorApplication.isPlaying = false;
